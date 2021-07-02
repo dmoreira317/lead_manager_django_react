331b6a41e92982fef31e03653b597e4f9832c3fc
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-07-01 21:40:39 -0300

    Prettified the JS code a bit

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,21 +1,20 @@
-import React, {Component, Fragment} from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
 
-import Header from './layout/Header';
-import Dashboard from './leads/Dashboard';
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
 
-
-class App extends Component{
-    render(){
-        return (
-            <Fragment>
-            <Header/>
-            <div className="container">
-                <Dashboard/>
-            </div>
-            </Fragment>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Leads extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Leads List</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Leads List</h1>
+      </div>
+    );
+  }
 }
 
-export default Leads
+export default Leads;
