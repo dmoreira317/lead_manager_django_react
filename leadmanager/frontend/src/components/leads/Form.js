518e54317a9c725d0aaf37b7bc48e72c8f7f27c1
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-07-02 11:17:40 -0300

    Added form to create a new lead

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
 
+// When we have a form in react, we want each input to be part of the state of the component.
+
 export class Form extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
   render() {
+    const { name, email, message } = this.state;
     return (
-      <div>
-        <h1>Add Lead Form</h1>
+      <div className="card card-body mt-4 mb-4">
+        <h2>Add Lead</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              onChange={this.onChange}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              onChange={this.onChange}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <label>Message</label>
+            <input
+              className="form-control"
+              type="text"
+              name="message"
+              onChange={this.onChange}
+              value={message}
+            />
+          </div>
+          <div className="form-group mt-2">
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
